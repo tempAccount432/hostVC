a0c04bd20329234bc98b5f58aeefc3cee91fded4
Aamil <aamilnakhuda@gmail.com>
2025-03-28 18:20:35 +0530

    link updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
     // Replace with your actual file URL
-    const fileUrl = 'https://yourdomain.com/path-to-your-setup-file.exe';
+    // const fileUrl = 'https://yourdomain.com/path-to-your-setup-file.exe';
+    const fileUrl = 'https://github.com/tempAccount432/hostVC/releases/download/v1.0.0/app_nano.exe';
     window.location.href = fileUrl;
 });
