41635053b99e297cc0cbe4c93755a27692ac6753
Aamil <aamilnakhuda@gmail.com>
2025-03-28 17:01:47 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    // Replace with your actual file URL
+    const fileUrl = 'https://yourdomain.com/path-to-your-setup-file.exe';
+    window.location.href = fileUrl;
+});
