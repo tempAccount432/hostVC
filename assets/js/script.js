e749e88b5ffab3edec1bb86f8ed786af88af267a
Aamil <aamilnakhuda@gmail.com>
2025-04-14 18:10:05 +0530

    new design

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navToggleFunc = function () {
+  navToggleBtn.classList.toggle("active");
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+navToggleBtn.addEventListener("click", navToggleFunc);
+overlay.addEventListener("click", navToggleFunc);
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navToggleFunc);
+}
+
+
+
+/**
+ * header active on scroll
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 10 ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+// document.getElementById('downloadlink').addEventListener('click', function () {
+//   // Replace with your actual file URL
+//   // const fileUrl = 'https://yourdomain.com/path-to-your-setup-file.exe';
+//   const fileUrl = 'https://github.com/tempAccount432/hostVC/releases/download/v1.1.0/app_nano_setup.exe';
+//   window.location.href = fileUrl;
+// });
